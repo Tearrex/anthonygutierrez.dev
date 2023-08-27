26ee316e85252fc2c071031328e7dd5ce6ee4f56
NarwhalG <rtmoddingteam@gmail.com>
2023-08-26 19:29:21 -0700

    fix experience page layout

--- src/views/ExpView.vue
+++ src/views/ExpView.vue
@@ -2,17 +2,20 @@
 import Employer from '../components/Employer.vue';
 </script>
 <template>
-  <h1>My Employment History</h1>
-  <div class="employernest">
-    <Employer employer="Golden State Assembly LLC" job-title="IT Support Specialist" image="/gsa.jpg" start="April 2022"
-      end="August 2023" />
+  <div>
+    <h1>My Employment History</h1>
+    <div class="employernest">
+      <Employer employer="Golden State Assembly" job-title="IT Support Specialist" image="/gsa.jpg" start="April 2022"
+        end="August 2023" />
 
-    <Employer employer="theCoderSchool" job-title="Code Coach" image="tcs.jpg" start="August 2023" end="PRESENT" />
+      <Employer employer="theCoderSchool" job-title="Code Coach" image="tcs.jpg" start="August 2023" end="PRESENT" />
+    </div>
+
+    <a href="/Resume.pdf" target="_blank">
+      Download Resume
+    </a>
+
   </div>
-
-  <a href="/Resume.pdf" target="_blank">
-    Download Resume
-  </a>
 </template>
 
 <style scoped>
@@ -23,7 +26,7 @@ h1 {
 .employernest {
   display: flex;
   flex-flow: column;
-  gap: 1.2rem;
+  gap: 1rem;
   margin-bottom: 2rem;
 }
 </style>
