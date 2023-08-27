da1e738d41e6298a2d4509c8dfb125296d3680bb
NarwhalG <rtmoddingteam@gmail.com>
2023-08-26 19:34:25 -0700

    tweak experience page layout

--- src/components/Employer.vue
+++ src/components/Employer.vue
@@ -12,9 +12,11 @@ defineProps({
     <div class="nest">
         <img :src="image" />
         <div class="text">
-            <h2>{{ employer }} <small>{{ start }} to {{ end }}</small></h2>
+            <h2>{{ employer }}</h2>
             <h4>{{ jobTitle }}</h4>
+            <!-- <small>{{ start }} to {{ end }}</small> -->
         </div>
+        <!-- <button class="expand">+</button> -->
     </div>
 </template>
 
@@ -23,15 +25,25 @@ defineProps({
     display: flex;
     flex-flow: row;
     gap: 0.8rem;
+    background-color: var(--color-border);
+    padding: 10px;
+    position: relative;
+}
+
+.expand {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
 }
 
 img {
-    width: 70px;
+    max-height: 70px;
     aspect-ratio: 1/1;
 }
 
 h2 {
-    color: #fff;
+    color: var(--color-heading);
 }
 
 h2 small {
